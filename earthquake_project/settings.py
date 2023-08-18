9fadb5ed53ae5e553f66c8ac34ee81d1cb0ad497
Mehmet-D02 <71323491+Mehmet-D02@users.noreply.github.com>
2023-08-18 18:16:10 +0300

    Add files via upload

--- earthquake_project/settings.py
+++ earthquake_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fn@b#de9u!j&lx9*)6+zhsq7)2r0dc=b2uic3m@y)gxt6d%s0*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['earthquake-app.onrender.com']
+ALLOWED_HOSTS = ['earthquake-app.onrender.com','127.0.0.1']
 
 
 # Application definition
